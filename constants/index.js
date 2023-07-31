06667fed5171e6f180cfdb8ba1aa17fb2ad91a8d
A.L.I <82636112+alich12-5@users.noreply.github.com>
2023-07-31 08:44:10 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x2134521c47c1df896eaa3427807f88c738a3aa80";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
